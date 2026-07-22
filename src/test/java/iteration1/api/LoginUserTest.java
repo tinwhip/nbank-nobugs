d8cbfa4afd68d52f58986382b4ff10965179ae04
alezakirov <aleksey.zakirov@mvideo.ru>
2026-07-22 23:31:51 +0300

    fix check style

--- src/test/java/iteration1/api/LoginUserTest.java
+++ src/test/java/iteration1/api/LoginUserTest.java
@@ -63,5 +63,6 @@ public class LoginUserTest extends BaseTest {
                                 customer -> customer.getUsername().equals(user.getUsername())
                         ).toList()
         ).hasSize(1);
+        System.out.println();
     }
 }
